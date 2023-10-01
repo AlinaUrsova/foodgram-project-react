1119771f5de8ed2e914e7147adf34824dea76240
Алина Урсова <alina.ursova@yandex.ru>
2023-10-01 23:49:02 +0300

    пароль

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 from django.contrib.auth import get_user_model
 from django.shortcuts import HttpResponse
 from djoser.views import UserViewSet
+from djoser.serializers import SetPasswordSerializer
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.response import Response
 from api.serializers import (TagSerializer, RecipeSerializer, 
@@ -12,6 +13,7 @@ from recipes.models import Tag, Recipe, Ingredient
 #from users.models import User
 from api.permissions import AuthorOrReadOnly, AdminOrReadOnly
 from rest_framework.permissions import SAFE_METHODS
+from rest_framework.decorators import action
 
 User = get_user_model()
 
@@ -54,5 +56,26 @@ class CustomUserViewSet(UserViewSet):
 
     quryset = User.objects.all()
     serializer_class = CustomUserSerializer
+
+    @action(detail=False, methods=['POST'])
+    def set_password(self, request):
+        serializer = SetPasswordSerializer(
+            data=request.data,
+            context={'request': request}
+        )
+
+        if serializer.is_valid():
+            current_password = serializer.validated_data['current_password']
+            new_password = serializer.validated_data['new_password']
+
+            if self.request.user.check_password(current_password):
+                self.request.user.set_password(new_password)
+                self.request.user.save()
+                return Response(status=204)
+            else:
+                return Response({'detail': 'Пароли не совпадают.'},
+                                status=400)
+        else:
+            return Response(serializer.errors, status=400)
 
 
